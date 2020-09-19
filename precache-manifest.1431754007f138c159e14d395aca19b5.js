81e1766ae1d39c6ff3fecd8a4c5d02e9897e6c98
Volodia <wova320@gmail.com>
2020-09-19 20:03:15 +0300

    usof build version

---
+++ precache-manifest.1431754007f138c159e14d395aca19b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c24b6db926ee11abfd817ef78af73fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d917dba30b9b06b30086",
+    "url": "/static/css/main.f0a46958.chunk.css"
+  },
+  {
+    "revision": "ebf048c0fc79ba908b4a",
+    "url": "/static/js/2.5f991501.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5f991501.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d917dba30b9b06b30086",
+    "url": "/static/js/main.3a156fad.chunk.js"
+  },
+  {
+    "revision": "3076b037e6276ad236a1",
+    "url": "/static/js/runtime-main.bf4bdaea.js"
+  }
+]);
